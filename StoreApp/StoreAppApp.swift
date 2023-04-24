c04b29dad20f53e6fa633ca526b7e415733c39ff
mina eid <minaeid6@gmail.com>
2023-04-24 15:53:10 +0200

    Initial Commit

---
+++ StoreApp/StoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreAppApp.swift
+//  StoreApp
+//
+//  Created by Mina on 24/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct StoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
